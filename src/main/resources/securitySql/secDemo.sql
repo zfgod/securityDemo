c15293d3ad818ffcef50b583932c591915ae9928
zhangfan <zhangfan@ycmedia.cn>
2016-11-01 14:03:17 +0800

    sql脚本更新

--- src/main/resources/securitySql/secDemo.sql
+++ src/main/resources/securitySql/secDemo.sql
@@ -2,7 +2,7 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.6.22-log : Database - secdemo
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -43,11 +43,11 @@ CREATE TABLE `resources` (
   `level` int(11) DEFAULT '0' COMMENT '资源等级：0 >1 > 2 >3',
   `description` varchar(200) DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 /*Data for the table `resources` */
 
-insert  into `resources`(`id`,`name`,`parentId`,`resKey`,`type`,`resUrl`,`level`,`description`) values (0,'未定义资源',0,'sysUndefine','0','undefine',0,'未定义资源都转为获取此权限'),(1,'权限资源管理',0,'resManage','1','/resManage',1,'权限资源管理菜单'),(2,'权限资源查询',1,'resQuery','2','/resManage/resQuery.do',2,'权限资源查看'),(3,'权限资源添加',1,'resAdd','3','/resManage/resAdd.do',2,'权限资源添加'),(4,'权限资源获取',1,'resFind','3','/resManage/resFind.do',2,'获取单个权限资源信息'),(5,'用户管理',0,'userManage','1','userManage',1,'用户模块'),(7,'用户查询',5,'userQuery','2','/userManage/uQuery.do',2,'查询用户信息'),(8,'资源权限修改',1,'resUpdate','3','/resManage/resUpdate.do',2,'修改资源权限信息'),(9,'用户查看',5,'userFind','3','/userManage/uFind.do',2,'获取用户信息'),(10,'用户修改',5,'userUpdate','3','/userManage/userUpdate.do',2,'修改用户信息'),(11,'角色管理',0,'roleManage','1','roleManage',1,'角色管理菜单'),(12,'角色列表查询',11,'roleQuery','2','/role/query.do',2,'角色列表信息获取'),(13,'角色添加',11,'roleAdd','3','/role/add.do',2,'添加角色'),(14,'角色查看',11,'roleFind','3','/role/find.do',2,'查看指定角色信息'),(15,'角色修改',11,'roleUpdate','3','/role/update.do',2,'修改角色定义'),(16,'角色授权',11,'bindRoleRes','3','/role/bindRoleRes.do',2,'角色与权限的绑定');
+insert  into `resources`(`id`,`name`,`parentId`,`resKey`,`type`,`resUrl`,`level`,`description`) values (0,'未定义资源',0,'sysUndefine','0','undefine',0,'未定义资源都转为获取此权限'),(1,'权限资源管理',0,'resManage','1','/resManage',1,'权限资源管理菜单'),(2,'权限资源查询',1,'resQuery','2','/resManage/resQuery.do',2,'权限资源查看'),(3,'权限资源添加',1,'resAdd','3','/resManage/resAdd.do',2,'权限资源添加'),(4,'权限资源获取',1,'resFind','3','/resManage/resFind.do',2,'获取单个权限资源信息'),(5,'用户管理',0,'userManage','1','userManage',1,'用户模块'),(7,'用户查询',5,'userQuery','2','/user/query.do',2,'查询用户信息'),(8,'权限资源修改',1,'resUpdate','3','/resManage/resUpdate.do',2,'修改资源权限信息'),(9,'用户查看',5,'userFind','3','/user/find.do',2,'获取用户信息'),(10,'用户修改',5,'userUpdate','3','/user/update.do',2,'修改用户信息'),(11,'角色管理',0,'roleManage','1','roleManage',1,'角色管理菜单'),(12,'角色列表查询',11,'roleQuery','2','/role/query.do',2,'角色列表信息获取'),(13,'角色添加',11,'roleAdd','3','/role/add.do',2,'添加角色'),(14,'角色查看',11,'roleFind','3','/role/find.do',2,'查看指定角色信息'),(15,'角色修改',11,'roleUpdate','3','/role/update.do',2,'修改角色定义'),(16,'角色授权',11,'bindRoleRes','3','/role/bindRoleRes.do',2,'角色与权限的绑定'),(17,'用户角色绑定',5,'userBindRole','3','/user/bindRole.do',2,'给用户添加角色');
 
 /*Table structure for table `resources_role` */
 
@@ -59,11 +59,11 @@ CREATE TABLE `resources_role` (
   `role_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=145 DEFAULT CHARSET=utf8;
 
 /*Data for the table `resources_role` */
 
-insert  into `resources_role`(`id`,`resc_id`,`role_id`) values (8,1,2),(9,2,2),(10,3,2),(11,4,2),(12,5,2),(22,1,1),(23,2,1),(24,3,1),(25,5,1),(26,4,1),(27,16,1),(28,12,1),(29,14,1),(30,13,1),(31,15,1);
+insert  into `resources_role`(`id`,`resc_id`,`role_id`) values (8,1,2),(9,2,2),(10,3,2),(11,4,2),(12,5,2),(129,1,1),(130,2,1),(131,3,1),(132,5,1),(133,4,1),(134,16,1),(135,12,1),(136,14,1),(137,13,1),(138,15,1),(139,8,1),(140,7,1),(141,17,1),(142,9,1),(143,11,1),(144,10,1);
 
 /*Table structure for table `role` */
 
@@ -76,11 +76,11 @@ CREATE TABLE `role` (
   `description` varchar(200) DEFAULT NULL COMMENT '描述',
   `enable` int(11) DEFAULT '1' COMMENT '1=可用，0不可用，-1删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `role` */
 
-insert  into `role`(`id`,`name`,`roleKey`,`description`,`enable`) values (1,'超级管理员1','ROLE_USER','admin',1);
+insert  into `role`(`id`,`name`,`roleKey`,`description`,`enable`) values (1,'超级管理员1','ROLE_USER','admin',1),(2,'系统管理者','sysManage','系统配置',1),(3,'角色管理者','roleManage','配置角色',1),(8,'test3','aa','aa',1);
 
 /*Table structure for table `serverinfo` */
 
@@ -117,11 +117,11 @@ CREATE TABLE `user` (
   `regTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`userId`,`userName`,`userPassword`,`userRealName`,`userPhone`,`userQQ`,`regTime`,`status`) values (1,'admin','admin','admin','0253526','32432','2016-10-11 13:48:45',NULL);
+insert  into `user`(`userId`,`userName`,`userPassword`,`userRealName`,`userPhone`,`userQQ`,`regTime`,`status`) values (1,'admin','admin','admin','0253526','32432','2016-10-24 16:23:53','1'),(2,'test','test','测试号','aaa','aa','2016-10-24 16:23:45','1');
 
 /*Table structure for table `user_role` */
 
@@ -132,11 +132,11 @@ CREATE TABLE `user_role` (
   `user_id` bigint(20) NOT NULL DEFAULT '0',
   `role_id` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_role` */
 
-insert  into `user_role`(`id`,`user_id`,`role_id`) values (1,1,1);
+insert  into `user_role`(`id`,`user_id`,`role_id`) values (2,1,1),(3,1,2),(4,2,3),(5,2,2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
